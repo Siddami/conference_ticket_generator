89f2fd9b34e9158c5e1b3850d807fbf3d51d7a73
Siddami <dharmmy621@gmail.com>
2025-02-12 23:44:41 +0100

    feat: add Toaster from react-hot-toast to layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
+import { Toaster } from 'react-hot-toast';
+
 
 export const Layout = ({children}) => {
   return (
     <div className='Layout'>
+        <Toaster position='top-right' />
         <Navbar />
         {children}
     </div>
