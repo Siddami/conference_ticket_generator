4098365e8d85a2b6529c61c68d9ab93bf75f1d83
Siddami <dharmmy621@gmail.com>
2025-02-13 03:28:48 +0100

    fix: remove Toaster from layout to add to App.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import Navbar from './Navbar';
-import { Toaster } from 'react-hot-toast';
-
 
 export const Layout = ({children}) => {
   return (
     <div className='Layout'>
-        <Toaster position='top-right' />
         <Navbar />
         {children}
     </div>
